9a0228a071204c2c1632f8149ca88939e6db2f9a
Bruce Chen <662005@qq.com>
2014-12-04 17:30:41 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,13 @@
 local luacv = require "luacv"
-local image = luacv.load_image("2M.PNG", "UNCHANGED")
+local image = luacv.load_image("2.png", "UNCHANGED")
 print(image:get_size())
+image:release_image()
 image:set_image_roi(100, 100, 100, 100)
+--[
 print(image:get_size())
+
+--[[
 image:save_image("222.png", {
-	['PNG_COMPRESSION'] = 0,
+    ['PNG_COMPRESSION'] = 0,
 })
+--]]
