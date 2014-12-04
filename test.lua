a6d590a2dccb2f6c91dbc80cf8dd3237f5e2c444
Littlebox222 <littlebox222@Littlebox222s-iMac.local>
2014-12-04 18:37:58 +0800

    update

--- test.lua
+++ test.lua
@@ -1,14 +1,12 @@
 local luacv = require "luacv"
 
-local image = luacv.load_image("files/test_2.png", "UNCHANGED")
-print(image:get_size())
---image:release_image()
+local image = luacv.load_image("files/test_3.png", "ANYCOLOR")
+
 image:set_image_roi(100, 100, 100, 100)
---[
-print(image:get_size())
+image:save_image("files/test_3_set_image_roi.png", {['PNG_COMPRESSION'] = 0})
 
-image:save_image("files/222.png", {
 
-	['PNG_COMPRESSION'] = 0,
-})
---]]
+image:line(10, 50, 90, 50, {255,255,255,0}, 10, 'CONNECTION_4')
+image:save_image("files/test_3_line.png")
+
+image:release_image()
